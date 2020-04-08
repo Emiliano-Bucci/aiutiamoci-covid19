5648165c0d16587377073b140df651ab0041aad0
Emiliano-Bucci <emiliano.bucci@outlook.com>
2020-04-08 11:34:09 +0200

    Adjust info

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -90,7 +90,7 @@ export const PageWrapper: React.FC<Props> = ({ children }) => {
           <p>
             In questo portale vengono elencate le attività che operano
             ancora in questo periodo di crisi effettuando consegne a
-            domicilio.
+            domicilio oppure consulenze a distanza.
           </p>
         </div>
 
